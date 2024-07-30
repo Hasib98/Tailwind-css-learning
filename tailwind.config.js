85422b143d2b3044733b24a67dd7d485ef3e7d0e
Hasib98 <phoenixoffire1998@gmail.com>
2024-07-30 23:23:35 +0600

    learning bout tailwind v2 and shadow added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,10 @@
+const colors = require('tailwindcss/colors')
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+    },
   },
   plugins: [],
   darkMode: "media"  //'media' or 'class'
